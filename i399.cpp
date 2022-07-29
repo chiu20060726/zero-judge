732bab8ef3e65bcc695626ff864cc2bc6f90aa7a
chiu20060726 <96660932+chiu20060726@users.noreply.github.com>
2022-07-30 01:17:40 +0800

    Update i399.cpp

--- i399.cpp
+++ i399.cpp
@@ -1,15 +1,15 @@
 # include <bits/stdc++.h>
 using namespace std;
 int main(){
-	int a[3];
-	int num[10]={0,0,0,0,0,0,0,0,0,0};
+	int a[3]; //輸入用的
+	int num[10]={0,0,0,0,0,0,0,0,0,0}; //找眾數
 	for(int n=0;n<3;n++){
-		cin>>a[n];
-		num[a[n]]++;
+		cin>>a[n]; //輸入
+		num[a[n]]++; //把輸入當索引值，並加一
 	}
-	sort(num,num+10);
+	sort(num,num+10); //排順序
 	cout<<num[9];
-	sort(a,a+3);
+	sort(a,a+3); //排順序
 	if(num[9]==3){
 		cout<<" "<<a[2];
 	}else if(num[9]==2){
